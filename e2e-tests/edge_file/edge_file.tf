8afe2743f2cf107b62c7b21c63d0cfcd503bffe6
grulicht <to.grulich@gmail.com>
2025-05-13 10:49:45 +0200

    chore(resources + docs): updated and added new

--- e2e-tests/edge_file/edge_file.tf
+++ e2e-tests/edge_file/edge_file.tf
@@ -7,7 +7,7 @@ resource "portainer_edge_group" "example_static" {
 
 resource "portainer_edge_stack" "string_example" {
   name            = var.edge_stack_name
-  stack_file_path = file(var.edge_stack_file_path)
+  stack_file_path = file("${path.module}/${var.edge_stack_file_path}")
   deployment_type = var.edge_stack_deployment_type
   edge_groups     = [portainer_edge_group.example_static.id]
   registries      = var.edge_stack_registries
