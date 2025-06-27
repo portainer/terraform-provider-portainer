043c89f75024c1a8077e9bd2ea3c369a488761f6
grulicht <to.grulich@gmail.com>
2025-06-27 13:18:33 +0200

    feat: add support for username and password authentication (jwt token) [#27]

--- e2e-tests/compose_convert/variables.tf
+++ e2e-tests/compose_convert/variables.tf
@@ -1,14 +1,14 @@
 variable "portainer_url" {
   description = "Default Portainer URL"
   type        = string
-  default     = ""
+  default     = "https://localhost:9443"
 }
 
 variable "portainer_api_key" {
   description = "Default Portainer Admin API Key"
   type        = string
   sensitive   = true
-  default     = ""
+  default     = "ptr_xrP7XWqfZEOoaCJRu5c8qKaWuDtVc2Zb07Q5g22YpS8="
 }
 
 variable "portainer_skip_ssl_verify" {
